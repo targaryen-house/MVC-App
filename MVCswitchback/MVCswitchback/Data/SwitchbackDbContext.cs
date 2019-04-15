c763440932407dce7dcfd5f5ee5dde8562c0d40d
iangifford <lightyagamie10@gmail.com>
2019-04-15 11:11:02 -0700

    Built out proof of life. now adding core logic

---
+++ MVCswitchback/MVCswitchback/Data/SwitchbackDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCswitchback.Data
+{
+    public class SwitchbackDbContext : DbContext
+    {
+        public SwitchbackDbContext(DbContextOptions<SwitchbackDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
