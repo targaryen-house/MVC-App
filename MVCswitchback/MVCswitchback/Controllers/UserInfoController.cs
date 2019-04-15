8da6a89086a6a7e8d4dddf4c0408df60459876f5
iangifford <lightyagamie10@gmail.com>
2019-04-15 12:00:08 -0700

    userinfocontroller()

--- MVCswitchback/MVCswitchback/Controllers/UserInfoController.cs
+++ MVCswitchback/MVCswitchback/Controllers/UserInfoController.cs
@@ -3,13 +3,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using MVCswitchback.Models;
 using MVCswitchback.Data;
 
 namespace MVCswitchback.Controllers
 {
-    public class HomeController : Controller
+    public class UserInfoController : Controller
     {
+        private readonly SwitchbackDbContext _context;
+
+        public UserInfoController(SwitchbackDbContext context)
+        {
+            _context = context;
+        }
+
+
+
         [HttpGet]
         public IActionResult Index()
         {
--- MVCswitchback/MVCswitchback/Controllers/UserReviewsController.cs
+++ MVCswitchback/MVCswitchback/Controllers/UserReviewsController.cs
@@ -3,17 +3,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using MVCswitchback.Models;
 using MVCswitchback.Data;
 
 namespace MVCswitchback.Controllers
 {
-    public class HomeController : Controller
+    public class UserReviewsController : Controller
     {
+        private readonly SwitchbackDbContext _context;
+
+        
+
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
+
     }
 }
