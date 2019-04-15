a1465400d27141f3004b20872bf7744cf512b3fb
iangifford <lightyagamie10@gmail.com>
2019-04-15 10:22:36 -0700

    usestaticfiles

--- MVCswitchback/MVCswitchback/Startup.cs
+++ MVCswitchback/MVCswitchback/Startup.cs
@@ -25,7 +25,7 @@ namespace MVCswitchback
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseStaticFiles();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
