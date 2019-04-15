a4654c86946c7f3613f1c60b063f5db3f178a24f
iangifford <lightyagamie10@gmail.com>
2019-04-15 13:56:15 -0700

    User Reviews Controller being built out

--- MVCswitchback/MVCswitchback/Controllers/UserReviewsController.cs
+++ MVCswitchback/MVCswitchback/Controllers/UserReviewsController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MVCswitchback.Data;
+using MVCswitchback.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MVCswitchback.Controllers
 {
@@ -11,14 +13,34 @@ namespace MVCswitchback.Controllers
     {
         private readonly SwitchbackDbContext _context;
 
-        
-
-        [HttpGet]
-        public IActionResult Index()
+        public UserReviewsController(SwitchbackDbContext context)
         {
-            return View();
+            _context = context;
         }
 
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.UserReviews.ToListAsync());
+        }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var userReview = await _context.UserReviews
+                    .FirstOrDefaultAsync(m => m.ID == id);
+            if (userReview == null)
+            {
+                return NotFound();
+            }
+            return View(userReview);
+        }
     }
 }
