81ba56703c26f3eb6d5ef3b8aec8a4e5607ec32f
Andrew Roska <andrew.l.roska@gmail.com>
2019-04-16 14:21:21 -0700

    Update syntax

--- MVCswitchback/WebAppTests/TrailModelTests.cs
+++ MVCswitchback/WebAppTests/TrailModelTests.cs
@@ -1,3 +1,4 @@
+using MVCswitchback.Models;
 using System;
 using Xunit;
 
@@ -6,9 +7,24 @@ namespace WebAppTests
     public class TrailModelTests
     {
         [Fact]
-        public void Test1()
+        public void CanGetTrailID()
         {
+            Trail trail = new Trail()
+            {
+                ID = 1
+            };
 
+            Assert.Equal(1, trail.ID);
         }
+
+        [Fact]
+        public void CanSetTrailID()
+        {
+            Trail trail = new Trail();
+            trail.ID = 2;
+
+            Assert.Equal(2, trail.ID);
+        }
+
     }
 }
