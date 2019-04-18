7b6d40a903a25b54ccb86fee034957afb5c7b06e
Chris <chrismorton.cm@gmail.com>
2019-04-17 21:46:57 -0700

    get trails by location and by id works

--- MVCswitchback/MVCswitchback/Controllers/TrailController.cs
+++ MVCswitchback/MVCswitchback/Controllers/TrailController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using MVCswitchback.Models;
+using MVCswitchback.Models.View_Models;
 using Newtonsoft.Json;
 
 
@@ -15,10 +16,15 @@ namespace MVCswitchback.Controllers
 
     public class TrailController : Controller
     {
-        public async Task<IActionResult> Index(float lat, float lon)
+        public async Task<IActionResult> Index(string searchString)
         {
-            Rootobject trails = await BackendAPI.GetTrailsAsync(lat, lon);
-            return View(trails);
+            List<Trail> trails = await BackendAPI.GetTrailsAsync(searchString);
+            TrailsRootVM trailList = new TrailsRootVM
+            {
+                Trails = trails
+            };
+            return View(trailList);
+            
         }
 
         /// <summary>
--- MVCswitchback/MVCswitchback/Models/Trail.cs
+++ MVCswitchback/MVCswitchback/Models/Trail.cs
@@ -7,64 +7,121 @@ using System.Threading.Tasks;
 namespace MVCswitchback.Models
 {
 
+    //public class Rootobject
+    //{
+    //    public Trail[] Trails { get; set; }
+    //    public int Success { get; set; }
+    //}
+
+    //public class Trail
+    //{
+    //    [JsonProperty("id")]
+    //    public int ID { get; set; }
+    //    [JsonProperty("name")]
+    //    public string Name { get; set; }
+    //    [JsonProperty("type")]
+    //    public string Type { get; set; }
+    //    [JsonProperty("summary")]
+    //    public string Summary { get; set; }
+    //    [JsonProperty("difficulty")]
+    //    public string Difficulty { get; set; }
+    //    [JsonProperty("stars")]
+    //    public float Stars { get; set; }
+    //    [JsonProperty("starVotes")]
+    //    public int StarVotes { get; set; }
+    //    [JsonProperty("location")]
+    //    public string Location { get; set; }
+    //    [JsonProperty("url")]
+    //    public string Url { get; set; }
+    //    [JsonProperty("imgSqSmall")]
+    //    public string ImgSqSmall { get; set; }
+    //    [JsonProperty("imgSmall")]
+    //    public string ImgSmall { get; set; }
+    //    [JsonProperty("imgSmallMed")]
+    //    public string ImgSmallMed { get; set; }
+    //    [JsonProperty("imgMedium")]
+    //    public string ImgMedium { get; set; }
+    //    [JsonProperty("length")]
+    //    public float Length { get; set; }
+    //    [JsonProperty("ascent")]
+    //    public int Ascent { get; set; }
+    //    [JsonProperty("descent")]
+    //    public int Descent { get; set; }
+    //    [JsonProperty("high")]
+    //    public int High { get; set; }
+    //    [JsonProperty("low")]
+    //    public int Low { get; set; }
+    //    [JsonProperty("longitude")]
+    //    public float Longitude { get; set; }
+    //    [JsonProperty("latitude")]
+    //    public float Latitude { get; set; }
+    //    [JsonProperty("conditionStatus")]
+    //    public string ConditionStatus { get; set; }
+    //    [JsonProperty("conditionDetails")]
+    //    public string ConditionDetails { get; set; }
+    //    [JsonProperty("conditionDate")]
+    //    public string ConditionDate { get; set; }
+    //}
+
+
     public class Rootobject
     {
         public Trail[] Trails { get; set; }
-        public int Success { get; set; }
     }
 
     public class Trail
     {
-        [JsonProperty("id")]
+        [JsonProperty("trailID")]
+        public int TrailID { get; set; }
+        [JsonProperty("ID")]
         public int ID { get; set; }
         [JsonProperty("name")]
         public string Name { get; set; }
-        [JsonProperty("type")]
+        [JsonProperty("Type")]
         public string Type { get; set; }
-        [JsonProperty("summary")]
+        [JsonProperty("Summary")]
         public string Summary { get; set; }
-        [JsonProperty("difficulty")]
+        [JsonProperty("Difficulty")]
         public string Difficulty { get; set; }
-        [JsonProperty("stars")]
+        [JsonProperty("Stars")]
         public float Stars { get; set; }
-        [JsonProperty("starVotes")]
+        [JsonProperty("StarVotes")]
         public int StarVotes { get; set; }
-        [JsonProperty("location")]
+        [JsonProperty("Location")]
         public string Location { get; set; }
-        [JsonProperty("url")]
-        public string Url { get; set; }
-        [JsonProperty("imgSqSmall")]
+        [JsonProperty("URL")]
+        public string URL { get; set; }
+        [JsonProperty("ImgSqSmall")]
         public string ImgSqSmall { get; set; }
-        [JsonProperty("imgSmall")]
+        [JsonProperty("ImgSmall")]
         public string ImgSmall { get; set; }
-        [JsonProperty("imgSmallMed")]
+        [JsonProperty("ImgSmallMed")]
         public string ImgSmallMed { get; set; }
-        [JsonProperty("imgMedium")]
+        [JsonProperty("ImgMedium")]
         public string ImgMedium { get; set; }
-        [JsonProperty("length")]
+        [JsonProperty("Length")]
         public float Length { get; set; }
-        [JsonProperty("ascent")]
+        [JsonProperty("Ascent")]
         public int Ascent { get; set; }
-        [JsonProperty("descent")]
+        [JsonProperty("Descent")]
         public int Descent { get; set; }
-        [JsonProperty("high")]
+        [JsonProperty("High")]
         public int High { get; set; }
-        [JsonProperty("low")]
+        [JsonProperty("Low")]
         public int Low { get; set; }
-        [JsonProperty("longitude")]
+        [JsonProperty("Longitude")]
         public float Longitude { get; set; }
-        [JsonProperty("latitude")]
+        [JsonProperty("Latitude")]
         public float Latitude { get; set; }
-        [JsonProperty("conditionStatus")]
+        [JsonProperty("ConditionStatus")]
         public string ConditionStatus { get; set; }
-        [JsonProperty("conditionDetails")]
+        [JsonProperty("ConditionDetails")]
         public string ConditionDetails { get; set; }
-        [JsonProperty("conditionDate")]
+        [JsonProperty("ConditionDate")]
         public string ConditionDate { get; set; }
+        [JsonProperty("UserRatings")]
+        public object UserRatings { get; set; }
     }
-
-
-
 
 
 
