5beb85a1c62e941a18d98de4b7880f40f8621a0d
iangifford <lightyagamie10@gmail.com>
2019-04-18 15:01:13 -0700

    Summary comments

--- MVCswitchback/MVCswitchback/Models/Services/TrailService.cs
+++ MVCswitchback/MVCswitchback/Models/Services/TrailService.cs
@@ -17,6 +17,11 @@ namespace MVCswitchback.Models.Services
             _context = context;
         }
 
+        /// <summary>
+        /// Retrieves the information from the users review for a selected trail and returns all related data
+        /// </summary>
+        /// <param name="id"> ID for the trail </param>
+        /// <returns> List of user comments and a review related to the selected trail </returns>
         public async Task<List<UserComments>> GetUserReviews(int id)
         {
             var list = await _context.UserComments
