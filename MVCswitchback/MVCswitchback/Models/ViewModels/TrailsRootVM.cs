0482d3f040d0bca59a1ef786dc30e1d94225023d
Andrew Roska <andrew.l.roska@gmail.com>
2019-04-18 09:31:03 -0700

    Fix VM issues

--- MVCswitchback/MVCswitchback/Models/ViewModels/TrailsRootVM.cs
+++ MVCswitchback/MVCswitchback/Models/ViewModels/TrailsRootVM.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MVCswitchback.Models.View_Models
+namespace MVCswitchback.Models.ViewModels
 {
     public class TrailsRootVM
     {
