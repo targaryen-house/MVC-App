c5de7c530bdcc0b6f4debf326bbb99e92caa4ed6
iangifford <lightyagamie10@gmail.com>
2019-04-17 17:13:56 -0700

    UserInfo tests passing

--- MVCswitchback/MVCswitchback/Models/Trail.cs
+++ MVCswitchback/MVCswitchback/Models/Trail.cs
@@ -17,48 +17,70 @@ namespace MVCswitchback.Models
     {
         [JsonProperty("id")]
         public int ID { get; set; }
+
         [JsonProperty("name")]
         public string Name { get; set; }
+
         [JsonProperty("type")]
         public string Type { get; set; }
+
         [JsonProperty("summary")]
         public string Summary { get; set; }
+
         [JsonProperty("difficulty")]
         public string Difficulty { get; set; }
+
         [JsonProperty("stars")]
         public float Stars { get; set; }
+
         [JsonProperty("starVotes")]
         public int StarVotes { get; set; }
+
         [JsonProperty("location")]
         public string Location { get; set; }
+
         [JsonProperty("url")]
         public string Url { get; set; }
+
         [JsonProperty("imgSqSmall")]
         public string ImgSqSmall { get; set; }
+
         [JsonProperty("imgSmall")]
         public string ImgSmall { get; set; }
+
         [JsonProperty("imgSmallMed")]
         public string ImgSmallMed { get; set; }
+
         [JsonProperty("imgMedium")]
         public string ImgMedium { get; set; }
+
         [JsonProperty("length")]
         public float Length { get; set; }
+
         [JsonProperty("ascent")]
         public int Ascent { get; set; }
+
         [JsonProperty("descent")]
         public int Descent { get; set; }
+
         [JsonProperty("high")]
         public int High { get; set; }
+
         [JsonProperty("low")]
         public int Low { get; set; }
+
         [JsonProperty("longitude")]
         public float Longitude { get; set; }
+
         [JsonProperty("latitude")]
         public float Latitude { get; set; }
+
         [JsonProperty("conditionStatus")]
         public string ConditionStatus { get; set; }
+
         [JsonProperty("conditionDetails")]
         public string ConditionDetails { get; set; }
+
         [JsonProperty("conditionDate")]
         public string ConditionDate { get; set; }
     }
